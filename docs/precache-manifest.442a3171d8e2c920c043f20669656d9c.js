4310980da2b6c7cfeb097ecd1c6af14e8634765b
XanxusX <tako30215@hotmail.com>
2020-06-05 01:42:43 +0800

    bbb

---
+++ docs/precache-manifest.442a3171d8e2c920c043f20669656d9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b902782965d4f59a6c24",
+    "url": "/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1b6744ed6e1ae8ab882538fdb257e6af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c69728becf5de4590a",
+    "url": "/js/about.cdae38de.js"
+  },
+  {
+    "revision": "b902782965d4f59a6c24",
+    "url": "/js/app.43f98277.js"
+  },
+  {
+    "revision": "d6162612300f65a0b394",
+    "url": "/js/chunk-vendors.8f1306cf.js"
+  },
+  {
+    "revision": "85143b9c6c276295a334da38781d235e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
